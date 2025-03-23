463e683883724af2762d1b8d559cf8572cb0a6b9
Conrad132 <Caerckert@mail.fhsu.edu>
2025-03-22 22:43:54 -0500

    Completd building container

---
+++ config/Database.php
@@ -0,0 +1,28 @@
+<?php
+	class Database {
+		//DB Params
+		private $host = 'localhost';
+		private $port = '5432';
+		private $db_name = 'quotesdb';
+		private $username = 'postgres';
+		private $password = 'postgres';
+		private $conn;
+
+
+		//DB Connect
+		public function connect() {
+			$this->conn = null;
+
+			$dsn = "pgsql:host={$this->host};port={$this->port};dbname={$this->db_name}";
+
+			try {
+				$this->conn = new PDO($dsn, $this->username, $this->password);
+
+				$this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			} catch(PDOException $e) {
+				die(json_encode(['message' => 'Connection Error: ' . $e->getMessage()]));
+			}
+			return $this->conn;
+		}
+	}
+
