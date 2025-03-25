97c9f22abaaab59a50dd675583db8b17c691e753
Conrad132 <Caerckert@mail.fhsu.edu>
2025-03-25 17:56:29 -0500

    updated read-single to fix unexpected s

--- api/quotes/read_single.php
+++ api/quotes/read_single.php
@@ -19,14 +19,16 @@ $quote->id = isset($_GET['id']) ? $_GET['id'] : die();
 // Get quote
 if ($quote->id) {
     if ($quote->read_single()) {
-        echo json_encode(array(
+        $quote_array = array(
             'id' => $quote->id,
             'quote' => $quote->quote,
             'author' => $quote->author_name,
             'category' => $quote->category_name
-        ));
+        );
+        echo json_encode($quote_array);
     } else {
-        http_response_code(200);
+        // Quote not found
+        http_response_code(404);
         echo json_encode(array('message' => 'No Quotes Found'));
     }
 }
